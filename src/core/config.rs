7524e61907a66ad6b36b2fbe537f109fc1ca74a6
rjohnsen <johnsen@opendefinition.com>
2023-04-23 12:13:14 +0000

    Refining configuration file and project folder

--- src/core/config.rs
+++ src/core/config.rs
@@ -1,24 +1,37 @@
+use std::fs;
 use std::fs::File;
+use std::path::Path;
 use std::io::prelude::*;
 use serde::{ Serialize, Deserialize };
 
-#[derive(Serialize, Deserialize, Default)]
+#[derive(Serialize, Deserialize, Default, Debug)]
 pub struct Config {
     obsidian_vault: String
 }
 
 impl Config {
-    pub fn create_new(&mut self, path: std::path::PathBuf) -> std::io::Result<()> {
+    const MAIN_FOLDER: &str = "Schadenfreude";
+    const CONFIG_FILE: &str = ".config.toml";
+    
+    pub fn load(&mut self) {
+        let home_folder = dirs::home_dir().unwrap();
+        let main_folder = Path::new(&home_folder).join(Config::MAIN_FOLDER);
+        let config_file = Path::new(&main_folder).join(Config::CONFIG_FILE);
+
+        if main_folder.is_dir()  == false { fs::create_dir(&main_folder); }
+        if config_file.is_file() == false { self.create_config_file(&config_file); }
+
+        let content = std::fs::read_to_string(config_file).unwrap();    
+        let config: Config = toml::from_str::<Config>(&content).unwrap();
+        *self = config;
+    }
+
+    fn create_config_file(&mut self, path: &std::path::PathBuf) -> std::io::Result<()> {
         let binding = toml::to_string(&self).unwrap();
         let toml = binding.as_bytes();
         let mut file = File::create(path)?;
 
         file.write_all(toml)?;
         Ok(())
-    }
-
-    pub fn load(&mut self, path: std::path::PathBuf) {
-        let contents = std::fs::read_to_string(path);
-        println!("{:?}", contents);
     }
 }
--- src/core/project.rs
+++ src/core/project.rs
@@ -1,43 +1,8 @@
-extern crate dirs;
-
-use std::fs;
-use std::path::Path;
 use crate::core::config;
 
-const PROJECT_FOLDER: &str = "Schadenfreude";
-const CONFIG_FILE: &str = ".config.toml";
-
 pub fn load_project() {
-    if has_config_folder() == false {
-        create_config_folder();
-    }
+    let mut configuration = config::Config::default();
+    configuration.load(); 
 
-    if has_config_file() == false {
-        create_config_file();
-    }
-
-    let mut config = config::Config::default();
-
-    let home_dir = dirs::home_dir().unwrap();
-    let config_file_path = Path::new(&home_dir).join(PROJECT_FOLDER).join(CONFIG_FILE);
-    config.load(config_file_path);
-}
-
-fn has_config_folder() -> bool {
-    return Path::new(&dirs::home_dir().unwrap()).join(PROJECT_FOLDER).is_dir();
-}
-
-fn create_config_folder() {
-    fs::create_dir(Path::new(&dirs::home_dir().unwrap()).join(PROJECT_FOLDER));
-}
-
-fn has_config_file() -> bool {
-    return Path::new(&dirs::home_dir().unwrap()).join(PROJECT_FOLDER).join(CONFIG_FILE).is_file();
-}
-
-fn create_config_file() {
-    let home_dir = dirs::home_dir().unwrap();
-    let config_file_path = Path::new(&home_dir).join(PROJECT_FOLDER).join(CONFIG_FILE);
-    let mut config: config::Config = config::Config::default();
-    config.create_new(config_file_path);
+    println!("{:?}", configuration);
 }
